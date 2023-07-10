e73d3b1bc4cde181f9fde0c49dea2160a20c97be
josearcos2023 <jose.arcos@tecsup.edu.pe>
2023-07-10 03:40:15 -0500

    Seeders

--- app/Models/Respuesta.php
+++ app/Models/Respuesta.php
@@ -9,17 +9,6 @@ class Respuesta extends Model
 {
     use HasFactory;
 
-    // protected $fillable = [
-    //     'id_pregunta',
-    //     'respuesta',
-    //     'estado'
-    // ];
-
-    // public function preguntas()
-    // {
-    //     return $this->belongsTo(Pregunta::class,'id_pregunta','id_pregunta');
-    // }
-
     protected $fillable = [
         'id_pregunta',
         'respuesta',
